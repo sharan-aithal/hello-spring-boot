54faedb54053d1a017b329744304b1113d4d3361
Sharan Aithal <sharanaithal0@yahoo.com>
2021-06-27 16:45:11 +0530

    get mapping with optional params

--- src/main/java/io/aithal/hellospringboot/HelloController.java
+++ src/main/java/io/aithal/hellospringboot/HelloController.java
@@ -2,13 +2,19 @@ package io.aithal.hellospringboot;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String home(){
-        return "hello world";
+    public String home ( ) {
+        return "Hello World";
+    }
+
+    @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String hello ( @RequestParam(value = "name", defaultValue = "Stranger") String name ) {
+        return "Hello " + name;
     }
 }
