9c230ac3a28e84fd875a70fad10c3648708ef695
Sharan Aithal <sharanaithal0@yahoo.com>
2021-06-28 22:40:43 +0530

    endpoints now returns HashMap object

--- src/main/java/io/aithal/hellospringboot/HelloController.java
+++ src/main/java/io/aithal/hellospringboot/HelloController.java
@@ -5,16 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String home ( ) {
-        return "{\"message\": \"Hello World\"}";
+    public Map<String, String> home ( ) {
+        Map<String, String> res = new HashMap<> ();
+        res.put ( "message", "Hello World" );
+        return res;
     }
 
     @GetMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String hello ( @RequestParam(value = "name", defaultValue = "Stranger") String name ) {
-        return "{\"message\": \"Hello " + name + "\"}";
+    public Map<String, String> hello ( @RequestParam(value = "name", defaultValue = "Stranger") String name ) {
+        Map<String, String> res = new HashMap<> ();
+        res.put ( "message", "Hello " + name );
+        return res;
     }
 }
